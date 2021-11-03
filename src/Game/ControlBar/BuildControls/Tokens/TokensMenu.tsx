7ca302c378709cdfcf55e8e633d5dee450f109c1
notgoodatpickingnames <32577550+notgoodatpickingnames@users.noreply.github.com>
2021-11-03 00:23:35 -0500

    implementing Drag and Drop

--- src/Game/ControlBar/BuildControls/Tokens/TokensMenu.tsx
+++ src/Game/ControlBar/BuildControls/Tokens/TokensMenu.tsx
@@ -1,4 +1,5 @@
 import { makeStyles } from '@mui/styles';
+import { DragEvent, MouseEvent as SyntheticMouseEvent } from 'react';
 
 import { Vector2 } from '../../../../Utils/engine/Vector2';
 import { Token } from '../../../models/Token';
@@ -31,8 +32,13 @@ const tokens = [
 export function TokensMenu() {
     const classes = useStyles();
 
-    function onDragStart(event: any) {
-        console.log('DRAG ENTER', event, event.layerX, event.target);
+    function onDragStart(event: DragEvent) {
+        console.log('DRAG ENTER', event.target);
+        var rect: DOMRect = (event.target as any).getBoundingClientRect();
+        const relativeX = event.clientX - rect.left;
+        const relativeY = event.clientY - rect.top;
+
+        const mousePositionInImage = new Vector2(relativeX, relativeY);
     }
 
     return (
